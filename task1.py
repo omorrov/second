804131b7cd1eec50ed64deba215a59b31d363f25
roma <ramazan.omorov@mail.ru>
2020-06-25 20:33:56 +0600

    second commit

---
+++ task1.py
@@ -0,0 +1,7 @@
+
+num1=input('Введите число:')
+num2=input('Введите число:')
+сума=name[0]+'.'+surname
+# d=input('Добрый день' (name[0+'.']+(surname))
+print("Сумма равна:," +fullname)
+print('sad')
